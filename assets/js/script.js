78c49eced450405115a965f5525fad83078a6448
Mo Yang <moyang313@gmail.com>
2022-03-19 18:10:11 -0400

    update name references and add APIs to retrieve weather data back starting with city search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,104 @@
-var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
+var cityFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var lat = "";
+var lon = "";
 
-var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+//function to call API for city coordinates
+var getCityCoords = function(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=34d23521a96f3e289197e68214b41647";
+    // "https://api.openweathermap.org/data/2.5/weather?q=" + user + "&appid=34d23521a96f3e289197e68214b41647"
+    // https://api.github.com/users/" + user + "/repos
 
-    // make a request to the url
+    // // make a request to the url
     fetch(apiUrl)
     .then(function(response) {
         // request was successful
         if (response.ok) {
         response.json().then(function(data) {
-            displayRepos(data, user);
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            console.log(lat);
+            console.log(lon);
+            
+            getWeather(lat, lon);
         });
         } else {
-        alert('Error: GitHub User Not Found');
+        alert('Error: City Not Found');
         }
+        console.log(response);
     })
     .catch(function(error) {
         // Notice this `.catch()` getting chained onto the end of the `.then()` method
-        alert("Unable to connect to GitHub");
+        alert("Unable to connect, please try again later");
+    });
+};
+
+//function to call API for city weather using coordinates passed in from getCityCoords
+var getWeather = function(lat, lon) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=34d23521a96f3e289197e68214b41647";
+    //api.openweathermap.org/data/2.5/forecast?lat=39.4334&lon=-84.1666&appid=34d23521a96f3e289197e68214b41647
+    // "https://api.openweathermap.org/data/2.5/weather?q=" + user + "&appid=34d23521a96f3e289197e68214b41647"
+    // https://api.github.com/users/" + user + "/repos
+
+    //make a request to the url
+    fetch(apiUrl)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+        response.json().then(function(data) {
+            console.log(response);
+        });
+        } else {
+        alert('Error: City Not Found');
+        }
+        console.log(response);
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect, please try again later");
     });
 };
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
-    var username = nameInputEl.value.trim();
+    var cityname = cityInputEl.value.trim();
 
-    if (username) {
-    getUserRepos(username);
-    nameInputEl.value = "";
+    if (cityname) {
+    getCityCoords(cityname);
+    cityInputEl.value = "";
     } else {
-    alert("Please enter a GitHub username");
+    alert("Please enter a city name");
     }
     console.log(event);
 };
 
-var displayRepos = function(repos, searchTerm) {
-    // clear old content
-    repoContainerEl.textContent = "";
-    repoSearchTerm.textContent = searchTerm;
+// var displayRepos = function(cities, searchTerm) {
+//     // clear old content
+//     repoContainerEl.textContent = "";
+//     repoSearchTerm.textContent = searchTerm;
 
-    // loop over repos
-    for (var i = 0; i < repos.length; i++) {
-        // format repo name
-        var repoName = repos[i].owner.login + "/" + repos[i].name;
+//     // loop over repos
+//     for (var i = 0; i < cities.length; i++) {
+//         // format repo name
+//         var repoName = cities[i].owner.id + "/" + cities[i].name;
+//         console.log(repoName);
+//         // create a container for each repo
+//         var repoEl = document.createElement("div");
+//         repoEl.classList = "list-item flex-row justify-space-between align-center";
     
-        // create a container for each repo
-        var repoEl = document.createElement("div");
-        repoEl.classList = "list-item flex-row justify-space-between align-center";
+//         // create a span element to hold repository name
+//         var titleEl = document.createElement("span");
+//         titleEl.textContent = repoName;
     
-        // create a span element to hold repository name
-        var titleEl = document.createElement("span");
-        titleEl.textContent = repoName;
+//         // append to container
+//         repoEl.appendChild(titleEl);
     
-        // append to container
-        repoEl.appendChild(titleEl);
-    
-        // append container to the dom
-        repoContainerEl.appendChild(repoEl);
-    }
-};
+//         // append container to the dom
+//         repoContainerEl.appendChild(repoEl);
+//     }
+// };
 
-userFormEl.addEventListener("submit", formSubmitHandler);
+cityFormEl.addEventListener("submit", formSubmitHandler);
